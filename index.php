4284a74d3219df5779382a59bc54caa53ee7927f
elfinsky <projectxyzdk4649@users.noreply.github.com>
2021-02-18 16:42:58 +0900

    test_index

---
+++ index.php
@@ -0,0 +1,17 @@
+
+<!doctype html>
+<html lang="ja">
+<head>
+<meta charset="UTF-8">
+<title>form</title>
+</head>
+<body>
+    <p><a href="./ako.php">ako.php</a></p>
+    <p><a href="./biwako.php">biwako.php</a></p>
+    <p><a href="./insert_station.php">insert_station.php</a></p>
+    <p><a href="./jrkyoto.php">jrkyoto.php</a></p>
+    <p><a href="./kobesanyo.php">kobesanyo.php</a></p>
+    
+</body>
+</html>
+
