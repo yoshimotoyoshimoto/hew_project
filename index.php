66fdb259d3ef1ee3d1dce700596ede18a93f503b
elfinsky <projectxyzdk4649@users.noreply.github.com>
2021-02-18 16:47:35 +0900

    test

--- index.php
+++ index.php
@@ -6,11 +6,11 @@
 <title>form</title>
 </head>
 <body>
-    <p><a href="./ako.php">ako.php</a></p>
-    <p><a href="./biwako.php">biwako.php</a></p>
-    <p><a href="./insert_station.php">insert_station.php</a></p>
-    <p><a href="./jrkyoto.php">jrkyoto.php</a></p>
-    <p><a href="./kobesanyo.php">kobesanyo.php</a></p>
+    <p><a href="https://hew-project.herokuapp.com/ako.php">ako.php</a></p>
+    <p><a href="https://hew-project.herokuapp.com/biwako.php">biwako.php</a></p>
+    <p><a href="https://hew-project.herokuapp.com/insert_station.php">insert_station.php</a></p>
+    <p><a href="https://hew-project.herokuapp.com/jrkyoto.php">jrkyoto.php</a></p>
+    <p><a href="https://hew-project.herokuapp.com/kobesanyo.php">kobesanyo.php</a></p>
     
 </body>
 </html>
