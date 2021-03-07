edc8ec8d0c25721a3f045302ee9a1859bc1fa6af
yoshimoto_R <49577184+yoshimotoyoshimoto@users.noreply.github.com>
2021-03-08 07:30:50 +0900

    Update const.php

--- const.php
+++ const.php
@@ -1,6 +1,6 @@
 <?php
 //const 定数記入
-const DB_NAME = getenv("DB_NAME");
-const HOST = getenv("DB_HOSTNAME");
-const USER_ID = getenv("DB_USERNAME");
-const PASSWORD = getenv("DB_PASSWORD");
+const DB_NAME = 'heroku_ab577afd4c511e7';
+const HOST = 'us-cdbr-east-03.cleardb.com';
+const USER_ID = 'b94ec7f48ae1ba';
+const PASSWORD = '72e02fcd';
