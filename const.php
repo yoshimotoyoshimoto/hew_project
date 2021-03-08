6d5bc05c88f79b7dd4a0c3c3784d5f799b00430b
yoshimoto_R <49577184+yoshimotoyoshimoto@users.noreply.github.com>
2021-03-08 14:27:45 +0900

    Update const.php

--- const.php
+++ const.php
@@ -1,7 +1,7 @@
 <?php
 //const 定数記入
 class ConstClass {
-    const DB_NAME = getenv('DB_NAME');
+    const DB_NAME = getenv('DATA_NAME');
     const HOST = getenv('DB_HOSTNAME');
     const USER_ID = getenv('DB_USERNAME');
     const PASSWORD = getenv('DB_PASSWORD');
