0b4e9cea249edf4a12027f0872429840b6a0b5c6
elfinsky <projectxyzdk4649@users.noreply.github.com>
2021-03-08 11:29:25 +0900

    new

--- tpl/myline_insert.php
+++ tpl/myline_insert.php
@@ -1,12 +1,103 @@
 <!DOCTYPE html>
-<html lang="ja">
+<html>
 <head>
-<meta charset="utf-8">
-<link href="./css/style.css" rel="stylesheet">
-<title>マイ路線登録</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>列車走行位置</title>
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+    <style type="text/css">
+        .bs-component + .bs-component {
+          margin-top: 1rem;
+        }
+        @media (min-width: 768px) {
+            .bs-docs-section {
+            margin-top: 8em;
+            }
+            .bs-component {
+            position: relative;
+            }
+            .bs-component .modal {
+            position: relative;
+            top: auto;
+            right: auto;
+            bottom: auto;
+            left: auto;
+            z-index: 1;
+            display: block;
+            }
+            .bs-component .modal-dialog {
+            width: 90%;
+            }
+            .bs-component .popover {
+            position: relative;
+            display: inline-block;
+            width: 220px;
+            margin: 20px;
+            }
+            .nav-tabs {
+            margin-bottom: 15px;
+            }
+            .progress {
+            margin-bottom: 10px;
+            }
+        }
+    </style>
 </head>
-
 <body>
+<header>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary"　id="navbarColor01">
+            <div class="container">
+                <a class="navbar-brand" href="./">
+                <img src="./img/rail_top.png" width="30" height="30" class="d-inline-block align-top mr-1" alt="Honoka">
+                遅延情報.net
+                </a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbar">
+                    <ul class="navbar-nav mr-auto">
+                        <li class="nav-item">
+                            <a class="nav-link" href="./index.php">トップ<span class="sr-only">(current)</span></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="./yahoo.php">公式遅延情報<span class="sr-only">(current)</span></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="./bata_info.php">非公式遅延情報</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="./position_top.php">列車走行位置</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="./entry.php">新規会員登録</a>
+                        </li>
+                        <?php if(!isset($_SESSION['name'])){ ?>
+                        <li class="nav-item">
+                            <a class="nav-link" href="./login.php">ログイン</a>
+                        </li>
+                        <?php }else{ ?>
+                        <li class="nav-item">
+                            <a class="nav-link" href="./myline.php">お気に入り路線</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="./myline_insert.php">お気に入り路線登録</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="./login.php?logout=logout">ログアウト</a>
+                        </li>
+                        <?php } ?>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </header>
+    <main class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                
+            </div>
+        </div>
+    </main>
 <h1>マイ路線登録</h1>
 <p>お気に入り路線を登録し、すぐに確認することができます。</p>
 <?php if(isset($_SESSION['cant_insert'])){ ?>
