39bc32c903dfb62eb4d5d3724d17266c167682ca
elfinsky <projectxyzdk4649@users.noreply.github.com>
2021-03-08 12:00:00 +0900

    new

--- tpl/myline_insert.php
+++ tpl/myline_insert.php
@@ -3,8 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>列車走行位置</title>
+    <title>マイ路線登録</title>
     <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="css/top_b.css">
     <style type="text/css">
         .bs-component + .bs-component {
           margin-top: 1rem;
@@ -44,7 +45,8 @@
     </style>
 </head>
 <body>
-<header>
+    <div id="page_top"><a href="#"></a></div>
+    <header>
         <nav class="navbar navbar-expand-lg navbar-dark bg-primary"　id="navbarColor01">
             <div class="container">
                 <a class="navbar-brand" href="./">
@@ -94,64 +96,231 @@
     <main class="container">
         <div class="row">
             <div class="col-lg-12">
-                
+                <h1 class="my-5">マイ路線登録</h1>
+                <h2 class="mb-4" style="font-size: 1.4em; font-weight: 200;">地域別リンク</h2>
+                <div class="mb-4">
+                    <ul class="list-group">
+                        <li class="list-group-item"><a href="#hokaidou">北海道地方</a></li>
+                        <li class="list-group-item"><a href="#touhoku">東北地方</a></li>
+                        <li class="list-group-item"><a href="#kantou">関東地方</a></li>
+                        <li class="list-group-item"><a href="#tyubu">中部地方</a></li>
+                        <li class="list-group-item"><a href="#kinki">近畿地方</a></li>
+                        <li class="list-group-item"><a href="#tyugoku">中国地方</a></li>
+                        <li class="list-group-item"><a href="#sikoku">四国地方</a></li>
+                        <li class="list-group-item"><a href="#kyusyu">九州・沖縄地方</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <?php if(isset($_SESSION['cant_insert'])){ ?>
+        <p><?php echo $_SESSION['cant_insert']; ?></p>
+        <?php unset($_SESSION['cant_insert']); ?>
+        <?php } ?>
+        <div id="hokaidou" class="mb-5">
+            <h3 class="mb-3" style="font-size: 1.4em;">北海道地方</h3>
+            <div class="row">
+                <div class="col-lg-12">
+                    <table class="table table-bordered table-hover">
+                        <div class="bs-component">
+                            <tbody>
+                            <?php for ($i=0; $i < count($hokaidou_link); $i++){?>
+                                <tr class="table-info">
+                                <form action="./myline_insert.php" method="post">
+                                    <td><?php echo $hokaidou_link[$i][2]; ?><input type="hidden" name="line" value="<?php echo $hokaidou_link[$i][2]; ?>">
+                                        <div class="mx-3">
+                                            <button type="submit" class="btn btn-primary" name="line_insert" value="line_insert">登録する</button>
+                                        </div>
+                                    </td>
+                                </form>
+                                </tr>
+                            <?php } ?>
+                            </tbody>
+                        </div>
+                    </table>
+                </div>
+            </div>
+        </div>
+        <div id="touhoku" class="mb-5">
+            <h3 class="mb-3" style="font-size: 1.4em;">東北地方</h3>
+            <div class="row">
+                <div class="col-lg-12">
+                    <table class="table table-bordered table-hover">
+                        <div class="bs-component">
+                            <tbody>
+                            <?php for ($i=0; $i < count($touhoku_link); $i++){?>
+                                <tr class="table-info">
+                                <form action="./myline_insert.php" method="post">
+                                    <td><?php echo $touhoku_link[$i][2]; ?><input type="hidden" name="line" value="<?php echo $touhoku_link[$i][2]; ?>">
+                                        <div class="mx-3">
+                                            <button type="submit" class="btn btn-primary" name="line_insert" value="line_insert">登録する</button>
+                                        </div>
+                                    </td>
+                                </form>
+                                </tr>
+                            <?php } ?>
+                            </tbody>
+                        </div>
+                    </table>
+                </div>
+            </div>
+        </div>
+        <div id="kantou" class="mb-5">
+            <h3 class="mb-3" style="font-size: 1.4em;">関東地方</h3>
+            <div class="row">
+                <div class="col-lg-12">
+                    <table class="table table-bordered table-hover">
+                        <div class="bs-component">
+                            <tbody>
+                            <?php for ($i=0; $i < count($kantou_link); $i++){?>
+                                <tr class="table-info">
+                                <form action="./myline_insert.php" method="post">
+                                    <td><?php echo $kantou_link[$i][2]; ?><input type="hidden" name="line" value="<?php echo $kantou_link[$i][2]; ?>">
+                                        <div class="mx-3">
+                                            <button type="submit" class="btn btn-primary" name="line_insert" value="line_insert">登録する</button>
+                                        </div>
+                                    </td>
+                                </form>
+                                </tr>
+                            <?php } ?>
+                            </tbody>
+                        </div>
+                    </table>
+                </div>
+            </div>
+        </div>
+        <div id="tyubu" class="mb-5">
+            <h3 class="mb-3" style="font-size: 1.4em;">中部地方</h3>
+            <div class="row">
+                <div class="col-lg-12">
+                    <table class="table table-bordered table-hover">
+                        <div class="bs-component">
+                            <tbody>
+                            <?php for ($i=0; $i < count($tyubu_link); $i++){?>
+                                <tr class="table-info">
+                                <form action="./myline_insert.php" method="post">
+                                    <td><?php echo $tyubu_link[$i][2]; ?><input type="hidden" name="line" value="<?php echo $tyubu_link[$i][2]; ?>">
+                                        <div class="mx-3">
+                                            <button type="submit" class="btn btn-primary" name="line_insert" value="line_insert">登録する</button>
+                                        </div>
+                                    </td>
+                                </form>
+                                </tr>
+                            <?php } ?>
+                            </tbody>
+                        </div>
+                    </table>
+                </div>
+            </div>
+        </div>
+        <div id="kinki" class="mb-5">
+            <h3 class="mb-3" style="font-size: 1.4em;">近畿地方</h3>
+            <div class="row">
+                <div class="col-lg-12">
+                    <table class="table table-bordered table-hover">
+                        <div class="bs-component">
+                            <tbody>
+                            <?php for ($i=0; $i < count($kinki_link); $i++){?>
+                                <tr class="table-info">
+                                <form action="./myline_insert.php" method="post">
+                                    <td><?php echo $kinki_link[$i][2]; ?><input type="hidden" name="line" value="<?php echo $kinki_link[$i][2]; ?>">
+                                        <div class="mx-3">
+                                            <button type="submit" class="btn btn-primary" name="line_insert" value="line_insert">登録する</button>
+                                        </div>
+                                    </td>
+                                </form>
+                                </tr>
+                            <?php } ?>
+                            </tbody>
+                        </div>
+                    </table>
+                </div>
+            </div>
+        </div>
+        <div id="tyugoku" class="mb-5">
+            <h3 class="mb-3" style="font-size: 1.4em;">中国地方</h3>
+            <div class="row">
+                <div class="col-lg-12">
+                    <table class="table table-bordered table-hover">
+                        <div class="bs-component">
+                            <tbody>
+                            <?php for ($i=0; $i < count($tyugoku_link); $i++){?>
+                                <tr class="table-info">
+                                <form action="./myline_insert.php" method="post">
+                                    <td><?php echo $tyugoku_link[$i][2]; ?><input type="hidden" name="line" value="<?php echo $tyugoku_link[$i][2]; ?>">
+                                        <div class="mx-3">
+                                            <button type="submit" class="btn btn-primary" name="line_insert" value="line_insert">登録する</button>
+                                        </div>
+                                    </td>
+                                </form>
+                                </tr>
+                            <?php } ?>
+                            </tbody>
+                        </div>
+                    </table>
+                </div>
+            </div>
+        </div>
+        <div id="sikoku" class="mb-5">
+            <h3 class="mb-3" style="font-size: 1.4em;">四国地方</h3>
+            <div class="row">
+                <div class="col-lg-12">
+                    <table class="table table-bordered table-hover">
+                        <div class="bs-component">
+                            <tbody>
+                            <?php for ($i=0; $i < count($sikoku_link); $i++){?>
+                                <tr class="table-info">
+                                <form action="./myline_insert.php" method="post">
+                                    <td><?php echo $sikoku_link[$i][2]; ?><input type="hidden" name="line" value="<?php echo $sikoku_link[$i][2]; ?>">
+                                        <div class="mx-3">
+                                            <button type="submit" class="btn btn-primary" name="line_insert" value="line_insert">登録する</button>
+                                        </div>
+                                    </td>
+                                </form>
+                                </tr>
+                            <?php } ?>
+                            </tbody>
+                        </div>
+                    </table>
+                </div>
+            </div>
+            <div id="kyusyu" class="mb-5">
+            <h3 class="mb-3" style="font-size: 1.4em;">九州・沖縄地方</h3>
+            <div class="row">
+                <div class="col-lg-12">
+                    <table class="table table-bordered table-hover">
+                        <div class="bs-component">
+                            <tbody>
+                            <?php for ($i=0; $i < count($kyusyu_link); $i++){?>
+                                <tr class="table-info">
+                                <form action="./myline_insert.php" method="post">
+                                    <td><?php echo $kyusyu_link[$i][2]; ?><input type="hidden" name="line" value="<?php echo $kyusyu_link[$i][2]; ?>">
+                                        <div class="mx-3">
+                                            <button type="submit" class="btn btn-primary" name="line_insert" value="line_insert">登録する</button>
+                                        </div>
+                                    </td>
+                                </form>
+                                </tr>
+                            <?php } ?>
+                            </tbody>
+                        </div>
+                    </table>
+                </div>
             </div>
         </div>
     </main>
-<h1>マイ路線登録</h1>
-<p>お気に入り路線を登録し、すぐに確認することができます。</p>
-<?php if(isset($_SESSION['cant_insert'])){ ?>
-<p><?php echo $_SESSION['cant_insert']; ?></p>
-<?php unset($_SESSION['cant_insert']); ?>
-<?php } ?>
-<h2>路線を選択して検索</h2>
-<h3>北海道地方</h3>
-<?php for ($i=0; $i < count($hokaidou_link); $i++){?>
-    <form action="./myline_insert.php" method="post">
-        <?php echo $hokaidou_link[$i][2]; ?><input type="hidden" name="line" value="<?php echo $hokaidou_link[$i][2]; ?>"><button type="submit" name="line_insert" value="line_insert">登録する</button><br>
-    </form>
-<?php } ?>
-<h3>東北地方</h3>
-<?php for ($i=0; $i < count($touhoku_link); $i++){?>
-    <form action="./myline_insert.php" method="post">
-        <?php echo $touhoku_link[$i][2]; ?><input type="hidden" name="line" value="<?php echo $touhoku_link[$i][2]; ?>"><button type="submit" name="line_insert" value="line_insert">登録する</button><br>
-    </form>
-<?php } ?>
-<h3>関東地方</h3>
-<?php for ($i=0; $i < count($kantou_link); $i++){ ?>
-    <form action="./myline_insert.php" method="post">
-        <?php echo $kantou_link[$i][2]; ?><input type="hidden" name="line" value="<?php echo $kantou_link[$i][2]; ?>"><button type="submit" name="line_insert" value="line_insert">登録する</button><br>
-    </form>
-<?php } ?>
-<h3>中部地方</h3>
-<?php for ($i=0; $i < count($tyubu_link); $i++){?>
-    <form action="./myline_insert.php" method="post">
-        <?php echo $tyubu_link[$i][2]; ?><input type="hidden" name="line" value="<?php echo $tyubu_link[$i][2]; ?>"><button type="submit" name="line_insert" value="line_insert">登録する</button><br>
-    </form>
-<?php } ?>
-<h3>近畿地方</h3>
-<?php for ($i=0; $i < count($kinki_link); $i++){?>
-    <form action="./myline_insert.php" method="post">
-        <?php echo $kinki_link[$i][2]; ?><input type="hidden" name="line" value="<?php echo $kinki_link[$i][2]; ?>"><button type="submit" name="line_insert" value="line_insert">登録する</button><br>
-    </form>
-<?php } ?>
-<h3>中国地方</h3>
-<?php for ($i=0; $i < count($tyugoku_link); $i++){?>
-    <form action="./myline_insert.php" method="post">
-        <?php echo $tyugoku_link[$i][2]; ?><input type="hidden" name="line" value="<?php echo $tyugoku_link[$i][2]; ?>"><button type="submit" name="line_insert" value="line_insert">登録する</button><br>
-    </form>
-<?php } ?>
-<h3>四国地方</h3>
-<?php for ($i=0; $i < count($sikoku_link); $i++){?>
-    <form action="./myline_insert.php" method="post">
-        <?php echo $sikoku_link[$i][2]; ?><input type="hidden" name="line" value="<?php echo $sikoku_link[$i][2]; ?>"><button type="submit" name="line_insert" value="line_insert">登録する</button><br>
-    </form>
-<?php } ?>
-<h3>九州・沖縄地方</h3>
-<?php for ($i=0; $i < count($kyusyu_link); $i++){?>
-    <form action="./myline_insert.php" method="post">
-        <?php echo $kyusyu_link[$i][2]; ?><input type="hidden" name="line" value="<?php echo $kyusyu_link[$i][2]; ?>"><button type="submit" name="line_insert" value="line_insert">登録する</button><br>
-    </form>
-<?php } ?>
+    <div class="center_in">
+        <div class="card-body">
+            <blockquote class="blockquote mb-0">
+            <footer class="blockquote-footer">HEW-Special-Teams<cite title="Source Title">遅延情報.net</cite></footer>
+            </blockquote>
+        </div>  
+    </div>  
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="./js/bootstrap.min.js"></script>
+    <script type="text/javascript">
+    $('.bs-component [data-toggle="popover"]').popover();
+    $('.bs-component [data-toggle="tooltip"]').tooltip();
+    </script>
 </body>
 </html>
